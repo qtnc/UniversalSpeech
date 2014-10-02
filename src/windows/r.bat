a281d4205099dc64dce9ab5795bced174843f835
QuentinC-Github <webmaster@quentinc.net>
2014-10-02 20:33:31 +0200

    Fixed linking issue; libgcc no longer required as external DLL

--- src/windows/r.bat
+++ src/windows/r.bat
@@ -2,4 +2,4 @@
 del ..\..\obj\*.o
 for %%i in (*.c) do gcc %%i -w -s -O3 -c -std=gnu99 -o ..\..\obj\%%~ni.o
 for %%i in (..\*.c) do gcc %%i -w -s -O3 -Os -c -std=gnu99 -o ..\..\obj\%%~ni.o
-gcc ..\..\obj\*.o main.def -shared -s -O3 -o ..\..\bin\UniversalSpeech.dll -Wl,--out-implib,..\..\lib\libUniversalSpeech.a -Wl,--add-stdcall-alias -Wl,--enable-stdcall-fixup -lsapi -lole32 -loleaut32 -luuid -lpsapi -lversion
+gcc ..\..\obj\*.o main.def -shared -static-libgcc -s -O3 -o ..\..\bin\UniversalSpeech.dll -Wl,--out-implib,..\..\lib\libUniversalSpeech.a -Wl,--add-stdcall-alias -Wl,--enable-stdcall-fixup -lsapi -lole32 -loleaut32 -luuid -lpsapi -lversion
